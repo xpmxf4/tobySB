7cfb5902ede2b2a60e827ed1e39845790e68e468
dev-chpark <xpmxf5@gmail.com>
2023-09-01 14:58:52 +0900

    ComponentScan

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -4,27 +4,36 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 
+@Configuration
+@ComponentScan
 public class HellobootApplication {
     public static void main(String[] args) {
         // Spring Container 생성 및 Bean 등록, 초기화
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
-//        applicationContext.registerBean(ComplexHelloService.class);
-        applicationContext.refresh();
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+            @Override
+            protected void onRefresh() { // Spring Container 초기화 작업
+                super.onRefresh();
 
-
-        // Servlet Container 생성, DispatcherServlet 초기화
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatcherServlet",
-                    new DispatcherServlet(applicationContext)
-            ).addMapping("/*");
-        });
-        webServer.start();
+                // Servlet Container 생성, DispatcherServlet 초기화
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(this)
+                    ).addMapping("/*");
+                });
+                webServer.start();
+            }
+        };
+        applicationContext.register(HellobootApplication.class);
+        applicationContext.refresh(); //
     }
 }
---
+++ src/main/java/tobyspring/helloboot/MyComponent.java
@@ -0,0 +1,7 @@
+package tobyspring.helloboot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public @interface MyComponent {
+}
